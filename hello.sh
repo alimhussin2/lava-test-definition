59686bf8047d3c14006f8f9348c86bd06ffdf096
Mohamad Noor Alim bin Hussin <alimhussin2@gmail.com>
2017-09-06 10:27:44 +0800

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -7,3 +7,19 @@ set -e
 
 echo "hello world. This is a bash script!"
 
+for i in {1..10}
+do
+    echo "test-case-$i    ERROR "
+done
+
+for i in {11..20}
+do
+    echo "test-case-$i    FAIL "
+done
+
+for i in {21..30}
+do
+    echo "test-case-$i    PASS "
+done
+
+exit 0
